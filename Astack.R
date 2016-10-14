702975a0dae7737b6c03b3040cec8ce71bb91551
debarros <pj.debarros@gmail.com>
2016-10-13 20:05:48 -0400

    Astack function: Added messageLevel parameter and status messages

--- Astack.R
+++ Astack.R
@@ -2,7 +2,9 @@
 
 #Stacking 2 objects, each of which can be 2 or 3 dimensional
 
-Astack = function(x, y){
+Astack = function(x, y, messageLevel = 0){
+  
+  if(messageLevel > 0) message("starting Astack function")
   
   if(sum((dim(x)[1:2] == dim(y)[1:2])) < 2){
     print(paste0("Error: The first two dimensions of the two objects must match. dim(x) = ",dim(x)[1]," , ",dim(x)[2], 
@@ -17,6 +19,8 @@ Astack = function(x, y){
   if(length(dim(y)) == 2){y = array(data = y, dim = c(dim(y), 1))}  #if y is 2d, make it 3d
   
   z = array(data = c(x, y), dim = dim(x) + c(0,0,dim(y)[3]))        #stack the arrays
+  
+  if(messageLevel > 0) message("Ending Astack function")
   
   return(z)
   
