acf76d7b5755647a8920b1942ff29a148160cdf4
debarros <pj.debarros@gmail.com>
2016-10-13 20:08:39 -0400

    Build.Primary function: added messageLevel parameter and status messages; combined loops that call DataGrepl and DataDate into one loop that checks to see which one should be used; added code to remove z dummy variable and do garbage collection; fixed code to name the vertical dimension levels of M2.

--- Build.Primary.R
+++ Build.Primary.R
@@ -2,10 +2,15 @@
 
 #This should be broken up into those matches that require DataGrepl and those that can just use DatComp
 
-Build.Primary = function(Matcher, SMS, DirectCert){
+Build.Primary = function(Matcher, SMS, DirectCert, messageLevel = 0){
+  if(messageLevel > 0) message("starting Build.Primary function")
+
+  if(messageLevel > 1) message("set guardnames and streetnames")
   
   guardnames = length(grep(pattern = "gName", x = colnames(SMS), ignore.case = T)) #how many guardian name variable in SMS?
   streetnames = length(grep(pattern = "streetPart", x = colnames(SMS), ignore.case = T)) #how many street variable in SMS?
+  
+  if(messageLevel > 1) message("build sets of variables")
   
   sets = list()
   sets$ParentName = list("PS" = paste0("gName",1:guardnames), "DC" = c("Case.Name.Guardian"))
@@ -15,30 +20,43 @@ Build.Primary = function(Matcher, SMS, DirectCert){
   sets$City = list("PS" = c("Mailing_City","City","X2nd_mailing_city"), "DC" = c("City"))
   sets$DOB = list("PS" = c("Year","Month","Day"), "DC" = c("DOB"))
   
+  if(messageLevel > 1) message("make the data.frame showing all pairs of variables to be matched")
+  
   y = data.frame(Var1 = character(), Var2 = character(), stringsAsFactors = F)
   
   for(i in 1:6){
     x = expand.grid(sets[[i]]$PS, sets[[i]]$DC, stringsAsFactors = F)
     y = rbind(y,x)
+    gc()
   }
 
   M2 = Matcher
   
-  for(i in 1:(nrow(y)-3)){
-    print(paste0("Match #",i))
-    z = DataGrepl(Matcher, SMS, DirectCert, y[i,1], y[i,2])
-    M2 = Astack(M2, z)
-  }
+  if(messageLevel > 1) message("loop through all pairs of variables")
   
-  for(i in (nrow(y)-2):nrow(y)){
+  for(i in 1:(nrow(y))){
     print(paste0("Match #",i))
-    z = DataDate(Matcher, SMS, DirectCert, "DOB", y[i,2], datePart = y[i,1])
-    M2 = Astack(M2, z)
+    if(y[i,2] == "DOB") {
+      z = DataDate(Matcher, SMS, DirectCert, "DOB", y[i,2], datePart = y[i,1], messageLevel = messageLevel - 1)
+    } else {
+      z = DataGrepl(Matcher, SMS, DirectCert, y[i,1], y[i,2], messageLevel = messageLevel - 1)
+    }
+    M2 = Astack(M2, z, messageLevel = messageLevel -1)
+    gc()
   }
+  gc()
+  rm(list = "z")
+  gc()
   
+  if(messageLevel > 1) message("and now something with dimnames")
   
   v = dimnames(Matcher)[1:2]
-  v$Type = q[c(1:43,45,46,44)] = paste(y$Var1," X ", y$Var2)
+  v$Type = paste(y$Var1," X ", y$Var2)
   dimnames(M2) = v
+  
+  if(messageLevel > 0) message("Ending Build.Primary function")
+  
   return(M2)
 }
+
+
