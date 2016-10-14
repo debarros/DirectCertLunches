563205bda08f7e3159d09ab8db5a8ddeded36ba1
debarros <pj.debarros@gmail.com>
2016-10-13 20:12:12 -0400

    DataGrepl: added messageLevel parameter and status messages.

--- DataGrepl.R
+++ DataGrepl.R
@@ -10,12 +10,18 @@
 #  5) uses regular expressions to match patterns from each selected value from the first data.frame to each selected value of the second
 #  6) outputs a logical matrix showing which patterns from the first data.frame found matches in which values from the second
 
-DataGrepl = function(CompArray, row.data, col.data, firstdim, seconddim, dimensions = c(1,2)){
+DataGrepl = function(CompArray, row.data, col.data, firstdim, seconddim, dimensions = c(1,2), messageLevel = 0){
+  
+  if(messageLevel > 0) message("starting DataGrepl function")
+  
   row.info = row.data[,firstdim]
   col.info = col.data[,seconddim]
   q = row.info[match(x = unlist(dimnames(CompArray)[dimensions[1]]), table = rownames(row.data))]
   r = col.info[match(x = unlist(dimnames(CompArray)[dimensions[2]]), table = rownames(col.data))]
   output = t(Vgrepl(pattern = q, x = r))
   dimnames(output) = dimnames(CompArray)[dimensions]
+  
+  if(messageLevel > 0) message("Ending DataGrepl function")
+  
   return(output)
 }
