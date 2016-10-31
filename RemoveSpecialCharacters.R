6ddeafc78cb45f2d635b0879773d3792ca144b97
debarros <pj.debarros@gmail.com>
2016-10-31 15:16:24 -0400

    RemoveSpecialCharacters.R - replaced individual lines with a loop that runs through all the different name variables.  Modified function to add parameters Vars, nameForms, and messageLevel.

--- RemoveSpecialCharacters.R
+++ RemoveSpecialCharacters.R
@@ -1,18 +1,18 @@
 #RemoveSpecialCharacters
 #This function creates versions of the student names that remove special characters like spaces, hyphens, or apostrophes
 
-RemoveSpecialCharacters = function (Students){
-  #This just removes any special characters
-  Students$FirstRemoveCharacters = gsub("\\W","",Students$First_Name)
-  Students$LastRemoveCharacters = gsub("\\W","",Students$Last_Name)
-  
-  #This replaces special characters with spaces
-  Students$FirstSpaceCharacters = gsub("\\W"," ",Students$First_Name)
-  Students$LastSpaceCharacters = gsub("\\W"," ",Students$Last_Name)
-  
-  #this remove everything after any special character, and converts to all uppercase for exact matching
-  Students$FirstRemoveAfterCharacters = toupper(gsub("\\W\\w+"," ",Students$First_Name)) 
-  Students$LastRemoveAfterCharacters = toupper(gsub("\\W\\w+"," ",Students$Last_Name))
-  
+RemoveSpecialCharacters = function (Students, Vars, nameForms, messageLevel = 0){
+
+  for (i in 1:length(Vars)){
+    #This just removes any special characters
+    Students[,paste0(names(Vars[i]),"RemoveCharacters")] = gsub("\\W","",Students[,Vars[[i]]])
+    
+    #This replaces special characters with spaces
+    Students[,paste0(names(Vars[i]),"SpaceCharacters")] = gsub("\\W"," ",Students[,Vars[[i]]])
+    
+    #this remove everything after any special character, and converts to all uppercase for exact matching
+    Students[,paste0(names(Vars[i]),"RemoveAfterCharacters")] = toupper(gsub("\\W\\w+"," ",Students[,Vars[[i]]])) 
+  }
+
   return(Students)
 }
