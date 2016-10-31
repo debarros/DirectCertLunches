183c4258caeeed1cb363a8273dc52304f5bb4c1c
debarros <pj.debarros@gmail.com>
2016-10-31 15:20:41 -0400

    MissingZips.R - added zipVars parameter, rewrote function to determine whether zip codes were missing from the Medicaid or SNAP information.

--- MissingZips.R
+++ MissingZips.R
@@ -1,19 +1,34 @@
 #Find missing zips
 
-MissingZips = function (DirectCert, Students){
+MissingZips = function (DirectCert, Students, zipVars){
   
-  DCzips = unique(DirectCert$Zip)
-  PSzips = unique(c(Students$X2nd_mailing_zip, Students$Mailing_Zip, Students$Zip))
+  DCzips.snap = unique(DirectCert$Zip[DirectCert$Case.Type == "SNAP"])
+  DCzips.medi = unique(DirectCert$Zip[DirectCert$Case.Type == "MEDICAID"])
+  PSzips = unique(unlist(c(SMS[,zipVars])))
+  PSzips = PSzips[!is.na(PSzips)]
   
-  missing = PSzips[!(PSzips %in% DCzips)]
+  missing.snap = PSzips[!(PSzips %in% DCzips.snap)]
+  missing.medi = PSzips[!(PSzips %in% DCzips.medi)]
   
-  missing = missing[order(missing)]
+  missing.snap = missing.snap[order(missing.snap)]
+  missing.medi = missing.medi[order(missing.medi)]
   
-  missing = missing[!is.na(missing)]
   
-  if(length(missing) == 0){
+  if(length(missing.snap)+length(missing.medi) == 0){
     print("No missing zip codes")
   }else{
-    print(paste0(c("The following zip codes are missing from the Direct Cert files: ", missing), collapse = " "))
+    if(length(missing.snap) > 0){
+      print(paste0(c("The following zip codes are missing from the SNAP Direct Cert files: ", missing.snap), collapse = " "))
+    }
+    if(length(missing.medi) > 0){
+      print(paste0(c("The following zip codes are missing from the MEDICAID Direct Cert files: ", missing.snap), collapse = " "))
+    }
   }
+  
+  missing.all = unique(c(missing.medi, missing.snap))
+  missing.all = missing.all[order(missing.all)]
+  
+  gc()
+  
+  return(missing.all)
 }
