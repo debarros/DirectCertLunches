ce4dcfafd178c810b7da8a22e5ff66f6e98ad96d
debarros <pj.debarros@gmail.com>
2016-10-31 15:11:02 -0400

    VGrepl.R - added comments and garbage collection.  Added code to account for grepl returning NA's

--- Vgrepl.R
+++ Vgrepl.R
@@ -1,12 +1,22 @@
-#VbetterGrepl.R
+#VGrepl.R
 
 #### betterGrepl ####
+# This function behaves similarly to grepl,
+# except that, if the character vector has missing values,
+# betterGrepl returns FALSE instead of NA for that element
+
 betterGrepl = function(pattern, x, ignore.case = T){
+  
   y = grepl(pattern, x, ignore.case = ignore.case)
+  
+  rm(list = c("x", "pattern"))
+  gc()
+  
   if(sum(is.na(y))>0){
-    y[is.na(y)] = 0
-    y = as.logical(y)
+    y[is.na(y)] = FALSE
   }
+  gc()
+  
   return(y)
 }
 
